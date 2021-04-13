9839b2ab22dcc87c20af5996a4ddfee77e5eeecf
maxmusiichuk <musiichuk646@gmail.com>
2021-04-13 21:57:59 +0300

    ->Create simple structure of class

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <?php
 mb_internal_encoding("UTF-8");
-$url = 'https://gagadget.com/articles/';
-$content= file_get_contents($url);
+//$url = 'https://gagadget.com/articles/';
+//$content = file_get_contents($url);
 
-// get post title
-echo "<pre>";
-preg_match_all('/<h2.*class=\"b-nodetop__text\"><a href=\"(.*?)\">(.*?)<\/a>(.*?)<\/h2>/U', $content, $match_t);
-print_r($match_t[2]);
-echo "</pre>";
+class PostParser
+{
+    public function get_content(){
+        $url = '';
+        $get_content = file_get_contents($url);
+        return $get_content;
+    }
 
-// get post author
-echo "<pre>";
-preg_match_all('/<a.*href=\"(.+)\".*class=\"b-node-author__name\">(.+)<\/a>/U', $content, $match_n);
-print_r($match_n[2]);
-echo "</pre>";
+    public function get_title($url){
+        $content = $this->get_content($url);
+        preg_match_all('/<h2.*class=\"b-nodetop__text\"><a href=\"(.*?)\">(.*?)<\/a>(.*?)<\/h2>/U', $content, $match_t);
+        return $match_t[2];
+    }
 
-// get post creating date
-echo "<pre>";
-preg_match_all('/<span.*class=\"b-node-author__date\">(.*?)<\/span>/U', $content, $match_d);
-print_r($match_d[0]);
-echo "</pre>";
+    public function get_author($url){
+        $content = $this->get_content($url);
+        preg_match_all('/<a.*href=\"(.+)\".*class=\"b-node-author__name\">(.+)<\/a>/U', $content, $match_n);
+        return $match_n[2];
+    }
 
-//get post content not finished version
-//$content = trim(preg_replace('/\s\s+/', ' ', $content));
-preg_match_all('/<div.*class=\"b-node-list-item__text b-font-def\">\n\t\t\t\t\t<p>(.*)<\/p>\n\t\t\t\t<\/div>/U', $content, $match_c);
-print_r($match_c[0]);
+    public function get_date($url){
+        $content = $this->get_content($url);
+        preg_match_all('/<span.*class=\"b-node-author__date\">(.*?)<\/span>/U', $content, $match_d);
+        return $match_d[0];
+    }
+
+    public function get_post_content($url){
+        $content = $this->get_content($url);
+        preg_match_all('/<div.*class=\"b-node-list-item__text b-font-def\">\n\t\t\t\t\t<p>(.*)<\/p>\n\t\t\t\t<\/div>/U', $content, $match_c);
+        return $match_c[0];
+
+    }
+
+}
