267507b0068ef25ab13dec669deb03384e3195af
maxmusiichuk <musiichuk646@gmail.com>
2021-04-21 22:06:09 +0300

    -> change structure of project -> add class file

---
+++ PostParser.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class PostParser
+{
+
+    public $url;
+
+    public function get_content()
+    {
+        $url = $this->url;
+        $get_content = file_get_contents($url);
+        return $get_content;
+    }
+
+    public function get_title()
+    {
+        $content = $this->get_content();
+        preg_match_all('/<h2.*class=\"b-nodetop__text\"><a href=\"(.*?)\">(.*?)<\/a>(.*?)<\/h2>/U', $content, $match_t);
+        return $match_t[2];
+    }
+
+    public function get_author()
+    {
+        $content = $this->get_content();
+        preg_match_all('/<a.*href=\"(.+)\".*class=\"b-node-author__name\">(.+)<\/a>/U', $content, $match_n);
+        return $match_n[2];
+    }
+
+    public function get_date()
+    {
+        $content = $this->get_content();
+        preg_match_all('/<span.*class=\"b-node-author__date\">(.*?)<\/span>/U', $content, $match_d);
+        return $match_d[0];
+    }
+
+    public function get_post_content()
+    {
+        $content = $this->get_content();
+        preg_match_all('/<div.*class=\"b-node-list-item__text b-font-def\">\n\t\t\t\t\t<p>(.*)<\/p>\n\t\t\t\t<\/div>/U', $content, $match_c);
+        return $match_c[0];
+    }
+
+}
--- index.php
+++ index.php
@@ -1,47 +1,7 @@
 <?php
 mb_internal_encoding("UTF-8");
 
-class PostParser
-{
-
-    public $url;
-
-    public function get_content()
-    {
-        $url = $this->url;
-        $get_content = file_get_contents($url);
-        return $get_content;
-    }
-
-    public function get_title()
-    {
-        $content = $this->get_content();
-        preg_match_all('/<h2.*class=\"b-nodetop__text\"><a href=\"(.*?)\">(.*?)<\/a>(.*?)<\/h2>/U', $content, $match_t);
-        return $match_t[2];
-    }
-
-    public function get_author()
-    {
-        $content = $this->get_content();
-        preg_match_all('/<a.*href=\"(.+)\".*class=\"b-node-author__name\">(.+)<\/a>/U', $content, $match_n);
-        return $match_n[2];
-    }
-
-    public function get_date()
-    {
-        $content = $this->get_content();
-        preg_match_all('/<span.*class=\"b-node-author__date\">(.*?)<\/span>/U', $content, $match_d);
-        return $match_d[0];
-    }
-
-    public function get_post_content()
-    {
-        $content = $this->get_content();
-        preg_match_all('/<div.*class=\"b-node-list-item__text b-font-def\">\n\t\t\t\t\t<p>(.*)<\/p>\n\t\t\t\t<\/div>/U', $content, $match_c);
-        return $match_c[0];
-    }
-
-}
+include 'PostParser.php';
 
 $Parser = new PostParser;
 $Parser->url = "https://gagadget.com/articles/";
@@ -56,25 +16,27 @@ foreach ($title as $x => $x_value) {
     echo $x . "=" . $x_value;
     echo "<br>";
 }
-echo '<pre>';
 echo '</pre>';
+echo '<pre>';
 
 foreach ($author as $x => $x_value) {
     echo $x . "=" . $x_value;
     echo "<br>";
 }
-echo '<pre>';
 echo '</pre>';
+echo '<pre>';
 
 foreach ($date as $x => $x_value) {
     echo $x . "=" . $x_value;
     echo "<br>";
 }
-echo '<pre>';
 echo '</pre>';
+//echo '<pre>';
 
 foreach ($post_content as $x => $x_value) {
+//    $string = trim(preg_replace('/\r\n+/', ' ', $post_content));
+//    $string = trim(preg_replace('/\s\s+/', ' ', $post_content));
     echo $x . "=" . $x_value;
     echo "<br>";
 }
-echo '</pre>';
+//echo '</pre>';
